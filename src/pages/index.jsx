2a286f490de9a65095e669eaa03fb05d662d380a
Liubomyr <vasya19956a@gmail.com>
2022-05-17 14:35:16 +0300

    ADD structure

---
+++ src/pages/index.jsx
@@ -0,0 +1,9 @@
+export {default as AboutUs} from "./AboutUs";
+export {default as Event}from "./Event";
+export {default as Events}from "./Events";
+export {default as Home} from "./Home";
+export {default as Login}from "./Login";
+export {default as Managers}from "./Managers";
+export {default as Members} from "./Members";
+export {default as Profile}from "./Profile";
+export {default as Settings}from "./Settings";
