c6a758d3591149c535d3ccdaf2e8667c4405c9a5
Ivan <kovaletsivan@gmail.com>
2022-05-18 18:25:28 +0300

    inputs create

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
-import './App.scss';
+import { Login } from "./pages/Login";
+import { ForgotPassword } from "./pages/ForgotPassword";
+
+import "./App.scss";
 
 const App = () => {
-	return <div className='App'></div>;
+  return (
+    <div className="App">
+      <Login />
+      {/* <ForgotPassword /> */}
+    </div>
+  );
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.scss';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.scss";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-	<BrowserRouter>
-		<React.StrictMode>
-			<App />
-		</React.StrictMode>
-	</BrowserRouter>
+ReactDOM.render(
+  <BrowserRouter>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </BrowserRouter>,
+  document.getElementById("root")
 );
