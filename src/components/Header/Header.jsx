ea34fa85904a735d0e2795ffdf0d2cea3f81a41a
Volodymyr Shum <shum.vr@gmail.com>
2022-05-23 17:53:44 +0300

    Theme fix

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,107 +1,130 @@
-import { Link, NavLink, useNavigate } from "react-router-dom";
+import { Link, NavLink, useNavigate } from 'react-router-dom';
 
-import { useAuth } from "../../hook/useAuth";
+import { useSelector, useDispatch } from 'react-redux';
+import { toggleTheme } from '../../store/themeSlice';
 
-import "./Header.scss";
+import { useAuth } from '../../hook/useAuth';
+
+import './Header.scss';
 
 const Header = () => {
-  const navigate = useNavigate();
-  const moveToLogin = () => navigate("login", { replace: true });
-  const moveToProfile = () => navigate("profile");
+	const navigate = useNavigate();
+	const moveToLogin = () => navigate('login', { replace: true });
+	const moveToProfile = () => navigate('profile');
+	const theme = useSelector(state => state.theme.theme);
+	const dispatch = useDispatch();
 
-  const { isAuth, name, role } = useAuth();
+	const { isAuth, name, role } = useAuth();
 
-  const logout = () => {
-    sessionStorage.clear();
-  };
+	const logout = () => {
+		sessionStorage.clear();
+	};
 
-  return (
-    <div className={"header"}>
-      <div className={"container-xl"}>
-        <Link className={"header__logo pe-0 pe-md-3"} to="/">
-          <h1>logo</h1>
-        </Link>
-        <div className={"header__user pe-0 pe-md-3"}>
-          <div className={"header__theme pe-md-5"}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="25"
-              height="25"
-              fill="currentColor"
-              className="bi bi-moon"
-              viewBox="0 0 16 16"
-            >
-              <path d="M6 .278a.768.768 0 0 1 .08.858 7.208 7.208 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277.527 0 1.04-.055 1.533-.16a.787.787 0 0 1 .81.316.733.733 0 0 1-.031.893A8.349 8.349 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.752.752 0 0 1 6 .278zM4.858 1.311A7.269 7.269 0 0 0 1.025 7.71c0 4.02 3.279 7.276 7.319 7.276a7.316 7.316 0 0 0 5.205-2.162c-.337.042-.68.063-1.029.063-4.61 0-8.343-3.714-8.343-8.29 0-1.167.242-2.278.681-3.286z" />
-            </svg>
-          </div>
-          {isAuth ? (
-            <div className={"dropdown"}>
-              <div
-                className={"header__user  dropdown-toggle"}
-                role="button"
-                data-bs-toggle="dropdown"
-              >
-                <ul
-                  className="dropdown-menu"
-                  aria-labelledby="dropdownMenuLink"
-                >
-                  <li>
-                    <NavLink
-                      className="dropdown-item"
-                      to={"/profile"}
-                      onClick={moveToProfile}
-                    >
-                      Profile
-                    </NavLink>
-                  </li>
-                  <li>
-                    <NavLink
-                      className="dropdown-item"
-                      to={"/login"}
-                      onClick={() => {
-                        moveToLogin();
-                        logout();
-                      }}
-                    >
-                      Log out
-                    </NavLink>
-                  </li>
-                </ul>
-                <div className={"header__user_img pe-md-1"}>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="40"
-                    fill="currentColor"
-                    className="bi bi-person-circle"
-                    viewBox="0 0 16 16"
-                  >
-                    <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
-                    <path
-                      fillRule="evenodd"
-                      d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"
-                    />
-                  </svg>
-                </div>
-                <div className={"header__user_info d-xl-block ps-2"}>
-                  <div className={"header__user_name"}>{name}</div>
-                  <div className={"header__user_role small text-muted"}>
-                    {role}
-                  </div>
-                </div>
-              </div>
-            </div>
-          ) : (
-            <button
-              className="btn btn-primary d-sm-inline-block"
-              onClick={moveToLogin}
-            >
-              Login
-            </button>
-          )}
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className={'header'}>
+			<div className={'container-xl'}>
+				<Link className={'header__logo pe-0 pe-md-3'} to='/'>
+					<h1>logo</h1>
+				</Link>
+				<div className={'header__user pe-0 pe-md-3'}>
+					<div className={'header__theme pe-md-5'}>
+						<span
+							style={{ cursor: 'pointer' }}
+							onClick={() => dispatch(toggleTheme())}
+						>
+							{theme.isDark ? (
+								<svg
+									xmlns='http://www.w3.org/2000/svg'
+									width='25'
+									height='25'
+									fill='currentColor'
+									class='bi bi-brightness-high'
+									viewBox='0 0 16 16'
+								>
+									<path d='M8 11a3 3 0 1 1 0-6 3 3 0 0 1 0 6zm0 1a4 4 0 1 0 0-8 4 4 0 0 0 0 8zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z' />
+								</svg>
+							) : (
+								<svg
+									xmlns='http://www.w3.org/2000/svg'
+									width='25'
+									height='25'
+									fill='currentColor'
+									className='bi bi-moon'
+									viewBox='0 0 16 16'
+								>
+									<path d='M6 .278a.768.768 0 0 1 .08.858 7.208 7.208 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277.527 0 1.04-.055 1.533-.16a.787.787 0 0 1 .81.316.733.733 0 0 1-.031.893A8.349 8.349 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.752.752 0 0 1 6 .278zM4.858 1.311A7.269 7.269 0 0 0 1.025 7.71c0 4.02 3.279 7.276 7.319 7.276a7.316 7.316 0 0 0 5.205-2.162c-.337.042-.68.063-1.029.063-4.61 0-8.343-3.714-8.343-8.29 0-1.167.242-2.278.681-3.286z' />
+								</svg>
+							)}
+						</span>
+					</div>
+					{isAuth ? (
+						<div className={'dropdown'}>
+							<div
+								className={'header__user  dropdown-toggle'}
+								role='button'
+								data-bs-toggle='dropdown'
+							>
+								<ul
+									className='dropdown-menu'
+									aria-labelledby='dropdownMenuLink'
+								>
+									<li>
+										<NavLink
+											className='dropdown-item'
+											to={'/profile'}
+											onClick={moveToProfile}
+										>
+											Profile
+										</NavLink>
+									</li>
+									<li>
+										<NavLink
+											className='dropdown-item'
+											to={'/login'}
+											onClick={() => {
+												moveToLogin();
+												logout();
+											}}
+										>
+											Log out
+										</NavLink>
+									</li>
+								</ul>
+								<div className={'header__user_img pe-md-1'}>
+									<svg
+										xmlns='http://www.w3.org/2000/svg'
+										width='40'
+										fill='currentColor'
+										className='bi bi-person-circle'
+										viewBox='0 0 16 16'
+									>
+										<path d='M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z' />
+										<path
+											fillRule='evenodd'
+											d='M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z'
+										/>
+									</svg>
+								</div>
+								<div className={'header__user_info d-xl-block ps-2'}>
+									<div className={'header__user_name'}>{name}</div>
+									<div className={'header__user_role small text-muted'}>
+										{role}
+									</div>
+								</div>
+							</div>
+						</div>
+					) : (
+						<button
+							className='btn btn-primary d-sm-inline-block'
+							onClick={moveToLogin}
+						>
+							Login
+						</button>
+					)}
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Header;
