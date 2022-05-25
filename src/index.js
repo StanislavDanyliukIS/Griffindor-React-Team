bcfec84553d3063c49593a415cf310268a08ee7e
Liubomyr <vasya19956a@gmail.com>
2022-05-25 18:38:36 +0300

    ADD firebase

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
 import { Provider } from 'react-redux';
-import store from './store/store';
-import './index.scss';
-import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import store from './store/store';
+
+import App from './App';
+
+import './index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
