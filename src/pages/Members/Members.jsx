2ea5039e668310837e556a30e2f64ae7e8a694f8
Liubomyr <vasya19956a@gmail.com>
2022-05-26 18:44:16 +0300

    FIX a little bugs

--- src/pages/Members/Members.jsx
+++ src/pages/Members/Members.jsx
@@ -3,10 +3,11 @@ import MembersList from './components/MembersList/MembersList';
 import './Members.scss';
 import { useAuth } from '../../hook/useAuth';
 import MembersManagement from './components/MembersManagement/MembersManagement';
+import {useUserData} from "../../hook/useUserData";
 
 const Members = () => {
-	const { isAuth, role } = useAuth();
-
+	const { isAuth } = useAuth();
+	const { role } = useUserData();
 	return (
 		<div className={'members '}>
 			{isAuth && role === 'user' ? <MembersList /> : <MembersManagement />}
